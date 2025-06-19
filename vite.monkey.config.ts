b7050b639c65da80c294235849ebfe821720aec1
julie-seebauer <julie.seebauer@gmail.com>
2025-06-19 15:36:42 +0200

    add build:monkey in package.json to generate monkey-patching.js in dist

---
+++ vite.monkey.config.ts
@@ -0,0 +1,46 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
+import babel from 'vite-plugin-babel';
+import TurboConsole from 'unplugin-turbo-console/vite';
+
+const alias = {
+	'assets': resolve(__dirname, 'assets'),
+	'devtool': resolve(__dirname, 'devtool'),
+	'lang': resolve(__dirname, 'lang'),
+	'lib': resolve(__dirname, 'lib'),
+	'packages': resolve(__dirname, 'packages'),
+	'sandbox': resolve(__dirname, 'sandbox'),
+	'services': resolve(__dirname, 'services'),
+	'utils': resolve(__dirname, 'utils'),
+};
+
+export { alias };
+
+export default defineConfig({
+	plugins: [
+		vue(),
+		vueJsx(),
+		babel({ filter: /\.tsx?$/ }),
+		TurboConsole({ disableLaunchEditor: true }),
+	],
+	resolve: { alias },
+	build: {
+		lib: {
+			entry: resolve(__dirname, 'services/MonkeyService.ts'),
+			name: 'MonkeyPatching',
+			fileName: () => 'monkey-patching.js',
+			formats: ['es'],
+		},
+		rollupOptions: {
+			external: ['vue'],
+			output: {
+				globals: { vue: 'Vue' },
+				exports: 'named',
+				inlineDynamicImports: true,
+			},
+		},
+		emptyOutDir: false,
+	},
+});
