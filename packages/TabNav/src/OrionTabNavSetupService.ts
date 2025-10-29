730246895e10ad2f93df464df7decc5eccc91b9d
julie-seebauer <julie.seebauer@gmail.com>
2025-10-29 15:14:38 +0100

    OrionTabNav: refactor active tab logic to handle nested routes

--- packages/TabNav/src/OrionTabNavSetupService.ts
+++ packages/TabNav/src/OrionTabNavSetupService.ts
@@ -15,4 +15,10 @@ export default class OrionTabNavSetupService extends SharedSetupService {
 	constructor (protected props: OrionTabNavProps, protected emits: OrionTabNavEmits) {
 		super();
 	}
+
+	paneIsActive (pane: Private.TsxTabPane, useRouter = false): boolean {
+		return this.props.value === pane.props.name
+			|| (useRouter && ((this.router.currentRoute.value.name === pane.props.name)
+				|| (!!this.router.currentRoute.value.matched.find(x => x.name === this.router.currentRoute.value.name))));
+	}
 }
