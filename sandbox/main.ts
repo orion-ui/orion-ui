0741d0de31cda5a8f361cfbb0f968e9f5bede1a7
Jean-Brice Maisner <jb@maisner.fr>
2025-12-02 21:27:26 +0100

    fix sandbox style

--- sandbox/main.ts
+++ sandbox/main.ts
@@ -4,6 +4,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router/index.router';
 import Orion from '../lib';
+import '../packages/Shared/styles/styles.less';
 // import { orionAppServiceSingleton } from 'utils/Orion';
 
 const sandboxApp = createApp(App)
